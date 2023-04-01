689a2d932357ac1295c266db9f262d66f56c07ea
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-02 00:05:32 +0530

    New Header with Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate, useLocation, Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
+import logo from "../assets/logo.png";
 import {
   EuiHeader,
   EuiText,
@@ -12,7 +13,11 @@ import {
 } from "@elastic/eui";
 import { useAppSelector } from "../app/hooks";
 import { firebaseAuth } from "../lib/firebase";
-import { getCreatMeetingBreadCrumbs, getOneOnOneMeetingBreadCrumbs } from "../lib/breadCrumbs";
+import {
+  getCreatMeetingBreadCrumbs,
+  getOneOnOneMeetingBreadCrumbs,
+  getVideoConferenceBreadCrumbs,
+} from "../lib/breadCrumbs";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -20,7 +25,7 @@ const Header = () => {
   const username = useAppSelector((zoom) => zoom.auth.userInfo?.name);
   const [breadCrumbs, setBreadCrumbs] = useState([{ text: "Dashboard" }]);
   const [isResponsive, setIsResponsive] = useState(false);
-  const dsipatch = useDispatch();
+  const dispatch = useDispatch();
 
   const logout = () => {
     signOut(firebaseAuth);
@@ -32,6 +37,8 @@ const Header = () => {
       setBreadCrumbs(getCreatMeetingBreadCrumbs(navigate));
     else if (pathname === "/create1on1")
       setBreadCrumbs(getOneOnOneMeetingBreadCrumbs(navigate));
+    else if (pathname === "/videocon")
+      setBreadCrumbs(getVideoConferenceBreadCrumbs(navigate));
   }, [location, navigate]);
 
   const section = [
@@ -39,9 +46,7 @@ const Header = () => {
       items: [
         <Link to="/dashboard">
           <EuiText>
-            <h2 style={{ padding: "0 1vw", color: "white" }}>
-              Dev<span style={{ color: "#62CDFF" }}>Meets</span>
-            </h2>
+            <img src={logo} alt="" />
           </EuiText>
         </Link>,
       ],

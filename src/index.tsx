47d1a9172825affd0c907cc6c0264fc9c58bc2c6
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-01 11:46:57 +0530

    Main Typescript File

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { BrowserRouter } from 'react-router-dom'
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
